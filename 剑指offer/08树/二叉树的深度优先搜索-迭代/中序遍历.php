0e574c890f80d4dacd5ac7fe0284ab0ae0f00702
moonshadow <1815265375@qq.com>
2022-04-17 11:48:12 +0800

    完善了二叉树的前中后序遍历的笔记

--- 剑指offer/08树/二叉树的深度优先搜索-迭代/中序遍历.php
+++ 剑指offer/08树/二叉树的深度优先搜索-迭代/中序遍历.php
@@ -1,22 +1,24 @@
 <?php
+
 include('../创建二叉树.php');
-class Tree{
-	public $nodes = [];
-	public $stack = [];
-	public function inorderTraversal(Node $root){
-		$cur = $root;
-		while($cur != NULL || !empty($this->stack)){
-			while($cur != NULL){
-				array_push($this->stack,$cur);
-				$cur = $cur->left;
-			}
-			$cur = array_pop($this->stack);
-			array_push($this->nodes,$cur->data);
-			$cur = $cur->right;
-		}
-		return $this->nodes;
-	}
-	
+class Tree
+{
+    public $nodes = [];
+    public $stack = [];
+    public function inorderTraversal(Node $root)
+    {
+        $cur = $root;
+        while ($cur != null || !empty($this->stack)) {
+            while ($cur != null) {
+                array_push($this->stack, $cur);
+                $cur = $cur->left;
+            }
+            $cur = array_pop($this->stack);
+            array_push($this->nodes, $cur->data);
+            $cur = $cur->right;
+        }
+        return $this->nodes;
+    }
 }
 
 $a = new Node(1);
@@ -26,10 +28,14 @@ $d = new Node(4);
 $e = new Node(5);
 $f = new Node(6);
 $g = new Node(7);
-$a->buildTree($b,$c);
-$b->buildTree($d,$e);
-$c->buildTree($f,$g);
-
+$h = new Node(8);
+$i = new Node(9);
+$j = new Node(10);
+$a->buildTree($b, $c);
+$b->buildTree($d, $e);
+$c->buildTree($f, $g);
+$e->buildTree($h, $i);
+$g->left = $j;
 $tree = new Tree();
 $re = $tree->inorderTraversal($a);
 var_dump($re);
